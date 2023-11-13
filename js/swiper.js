3fcee576d689d46c3e4c52a064d65c1230cc55b1
SANHOKIM <awrko@naver.com>
2023-11-13 14:04:43 +0900

    배경화면+스와이프 콤비2

--- js/swiper.js
+++ js/swiper.js
@@ -1,24 +1,27 @@
 
 
 window.onload = function () {
-    const swiper = new Swiper('.swiper', {
+    const swiperMain = new Swiper('#mainBanner .swiper', {
         // Optional parameters
         loop: true,
 
         pagination: {
             el: '.swiper-pagination',
+            clickable: true,
         },
 
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+            nextEl: '#mainBanner .swiper-button-next',
+            prevEl: '#mainBanner .swiper-button-prev',
         },
         autoplay: {
-            delay: 500, //4초마다돌아갈게
+            delay: 2000, //4초마다돌아갈게
             disableOnInteraction: false,
             pauseOnMouseEnter: true,// 액션들어오면 받아드리지않을게
             // 마우스들어오면 멈출게 
+
         }
+
     });
 }
 
